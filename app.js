724608123944c3e89eca65b6fe1950edf355ea5e
Srijan Duggal <srijanduggal17@gmail.com>
2018-04-18 20:16:21 -0400

    Made basic program structure

--- app.js
+++ app.js
@@ -13,59 +13,49 @@ var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 var rp = require('request-promise');
 
+var stateKey = 'spotify_auth_state';
+var app = express();
+var code;
+
 var client_id = '15ec5ccbf8d648378ecefdf8bab3f58d'; // Your client id
 var client_secret = 'a40cc81bc12a4ea0adcb04a8638bd1f2'; // Your secret
 var redirect_uri = 'http://localhost:8889/callback/'; // Your redirect uri
 
+var trackobjectsarr = [];
 var mysongsarr = [];
 var myplaylistobjarr = [];
 
-/**
- * Generates a random string containing numbers and letters
- * @param  {number} length The length of the string
- * @return {string} The generated string
- */
- var generateRandomString = function(length) {
-	var text = '';
-	var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-	for (var i = 0; i < length; i++) {
-		text += possible.charAt(Math.floor(Math.random() * possible.length));
-	}
-	return text;
- };
-
-var stateKey = 'spotify_auth_state';
-
-var app = express();
-
-var code;
-
 app.use(express.static(__dirname + '/public'))
-.use(cookieParser());
+	.use(cookieParser());
 
 //Application requests authorization
 ////////////////Section 1 ////////////////////////////
-app.get('/login', function(req, res) {
+app.get('/login', postLogin);
+
+function postLogin(req, res) {
+	//This functions requests authorization to use the API
 	var state = generateRandomString(16);
 	res.cookie(stateKey, state);
 
-	  // your application requests authorization
-	  var scope = 'user-library-read';
+	var scope = 'user-library-read';
+	res.redirect('https://accounts.spotify.com/authorize?' +
+	querystring.stringify({
+		response_type: 'code',
+		client_id: client_id,
+		scope: scope,
+		redirect_uri: redirect_uri,
+		state: state
+	}));
+}
 
-	  res.redirect('https://accounts.spotify.com/authorize?' +
-		querystring.stringify({
-			response_type: 'code',
-			client_id: client_id,
-			scope: scope,
-			redirect_uri: redirect_uri,
-			state: state
-		}));
-});
+app.get('/callback', mainCallback);
 
-app.get('/callback', function(req, res) {
+function mainCallback(req, res) {
+	getInitialTokens(req, res)
+	.then(getMyData);
+}
 
-	//Response query string has code and state
+function getInitialTokens(req, res) {
 	code = req.query.code || null;
 	var state = req.query.state || null;
 	var storedState = req.cookies ? req.cookies[stateKey] : null;
@@ -80,6 +70,7 @@ app.get('/callback', function(req, res) {
 		//Request access and refresh tokens
 		///////////////////Section 2//////////////////////////////
 		var authOptions = {
+			method: 'POST',
 			url: 'https://accounts.spotify.com/api/token',
 			form: {
 				code: code,
@@ -92,33 +83,11 @@ app.get('/callback', function(req, res) {
 			json: true
 		};
 
-		request.post(authOptions, function(error, response, body) {
-			if (!error && response.statusCode === 200) {
+		return new Promise ((resolve, reject) => {
+			rp(authOptions)
+			.then(body => {
 				var access_token = body.access_token,
 				refresh_token = body.refresh_token;
-
-				let songlim = querystring.stringify({
-					limit: 50
-				});
-
-				var options = {
-					url: 'https://api.spotify.com/v1/me/tracks?' + songlim + '',
-					headers: { 'Authorization': 'Bearer ' + access_token },
-					json: true
-				};
-
-				// use the access token to access the Spotify Web API
-				//////////////Section 3////////////////////////
-				request.get(options, function(error, response, body) {
-					let totalsongs = body.total;
-
-					for (let i = 0; i < body.items.length; i++) {
-						mysongsarr.push(body.items[i].track.id);
-					}
-
-					getMySongs(totalsongs, access_token);
-					getMyPlaylistSongs(access_token);
-				});
 
 				// we can also pass the token to the browser to make requests from there
 				res.redirect('/#' +
@@ -126,48 +95,120 @@ app.get('/callback', function(req, res) {
 						access_token: access_token,
 						refresh_token: refresh_token
 					}));
-			} else {
+
+				var toks = [access_token, refresh_token];
+
+				resolve(toks);
+			})
+			.catch(error => {
+				console.error("Error obtaining access token from refresh token");
+				console.log(error);
+
 				res.redirect('/#' +
 					querystring.stringify({
 						error: 'invalid_token'
 					}));
-			}
+				reject("Access token not obtained");
+			});	
 		});
-	}
-});
-
-function getMySongs(tot, token) {
-	if (tot > 50) {
-		let prevoffset = 0;
-		let numtimes = Math.ceil((tot-50)/50);
-		let promarr = [];
-		for (let i = 0; i < numtimes; i++) {
-			let paramobj = {
-				limit: 50,
-				offset: prevoffset + 50
-			}; 
-			let params = querystring.stringify(paramobj);
-			prevoffset = paramobj.offset;
-
-			promarr.push(getTrackSet(params, token));
-		}
-
-		Promise.all(promarr)
-			.then(() => {
-				console.log("woohoo");
-				console.log(mysongsarr.length);
-			})
-			.catch(error => {
-				console.log("buabuabua");
-			});
 	}
 }
 
-function getTrackSet(para, toke) {
+function getMyData (toks) {
+	let savedtrackspromise = savedTracks(toks[0]);
+	let playlisttrackspromise = playlistTracks(toks[0]);
+	return new Promise((resolve, reject) => {
+		Promise.all([savedtrackspromise, playlisttrackspromise])
+		.then((results) => {
+			//combine into one array
+			resolve(results);
+		})
+	});
+}
+
+function savedTracks(token) {
+	return new Promise((resolve, reject) => {
+		getTotalSavedTracks(token)
+		.then(getSavedTrackObjects)
+		.then(arr => {
+			resolve(arr);
+		})
+	})
+}
+
+function getTotalSavedTracks(token) {
+	var options = {
+		method: 'GET',
+		url: 'https://api.spotify.com/v1/me/tracks?',
+		headers: { 'Authorization': 'Bearer ' + token },
+		json: true
+	};
+
+	return new Promise ((resolve, reject) => {
+		rp(options)
+		.then(body => {
+			console.log('Total saved tracks received');
+			let outObj = {
+				totalsongs: body.total,
+				token: token
+			}
+			resolve(outObj);
+		})
+		.catch(err => {
+			reject('Error getting total saved tracks');
+		});
+	});
+}
+
+function getSavedTrackObjects(inObj) {
+	let token = inObj.token;
+	let totalnum = inObj.totalsongs;
+
+	let objArray = [];
+	let promiseArr = [];
+
+	function addToObjArr(arr) {
+		objArray.push(arr);
+		resolve("added!");
+	}
+
+	let prevoffset = -50;
+	let numtimes = Math.ceil(totalnum/50);
+
+
+	for (let i = 0; i < numtimes; i++) {
+		let paramobj = {
+			limit: 50,
+			offset: prevoffset + 50
+		}; 
+		let params = querystring.stringify(paramobj);
+		prevoffset = paramobj.offset;
+
+		currentprom = new Promise((resolve, reject) => {
+			trackObjectRequest(token, params)
+			.then(addToObjArr)
+		});
+
+		promiseArr.push(currentprom);
+	}
+
+	return new Promise((resolve, reject) => {
+		Promise.all(promiseArr)
+			.then(() => {
+				resolve(objArray);
+			})
+			.catch(error => {
+				console.error("error");
+				reject("sorry");
+			});
+	});
+}
+
+function trackObjectRequest(token, params) {
 	let options = {
 		method: 'GET',
-		url: 'https://api.spotify.com/v1/me/tracks?' + para + '',
-		headers: { 'Authorization': 'Bearer ' + toke },
+		url: 'https://api.spotify.com/v1/me/tracks?' + params + '',
+		headers: { 'Authorization': 'Bearer ' + token },
 		json: true
 	};
 
@@ -175,10 +216,7 @@ function getTrackSet(para, toke) {
 		rp(options)
 			.then(body => {
 				let resultarr = body.items;
-				for (const ind in resultarr) {
-					mysongsarr.push(resultarr[ind].track.id)
-				}
-				resolve(body);
+				resolve(resultarr);
 			})
 			.catch(error => {
 				console.error("Error");
@@ -188,11 +226,89 @@ function getTrackSet(para, toke) {
 	});
 }
 
-function getMyPlaylistSongs(token) {
-	let params = querystring.stringify({
-		limit: 50
-	});
+function playlistTracks(token) {
+	return new Promise((resolve, reject) => {
+		getTotalPlaylists(token)
+		.then(getPlaylistObjects)
+		.then(getTotalPlaylistTrackObjects)
+		.then(arr => {
+			resolve(arr);
+		});
+	})
+}
 
+function getTotalPlaylists(token) {
+	var options = {
+		method: 'GET',
+		url: 'https://api.spotify.com/v1/me/playlists?',
+		headers: { 'Authorization': 'Bearer ' + token },
+		json: true
+	};
+
+	return new Promise ((resolve, reject) => {
+		rp(options)
+		.then(body => {
+			console.log('Total playlists received');
+			let outObj = {
+				totalplaylists: body.total,
+				token: token
+			}
+			resolve(outObj);
+		})
+		.catch(err => {
+			reject('Error getting total saved tracks');
+		});
+	});
+}
+
+function getPlaylistObjects(inObj) {
+	let totalplaylists = inObj.totalplaylists;
+	let token = inObj.token;
+
+	let objArray = [];
+	let promiseArr = [];
+
+	function addToObjArr(arr) {
+		objArray.push(arr);
+		resolve("added!");
+	}
+
+	let prevoffset = -50;
+	let numtimes = Math.ceil(totalplaylists/50);
+
+	for (let i = 0; i < numtimes; i++) {
+		let paramobj = {
+			limit: 50,
+			offset: prevoffset + 50
+		}; 
+		let params = querystring.stringify(paramobj);
+		prevoffset = paramobj.offset;
+
+		currentprom = new Promise((resolve, reject) => {
+			playlistObjectRequest(token, params)
+			.then(addToObjArr)
+		});
+
+		promiseArr.push(currentprom);
+	}
+
+	return new Promise((resolve, reject) => {
+		Promise.all(promiseArr)
+			.then(() => {
+				let outObj = {
+					token: token,
+					data: objArray
+				}
+				resolve(outObj);
+			})
+			.catch(error => {
+				console.error("error");
+				reject("sorry");
+			});
+	});
+}
+
+function playlistObjectRequest(token, params) {
 	let options = {
 		method: 'GET',
 		url: 'https://api.spotify.com/v1/me/playlists?' + params + '',
@@ -200,67 +316,11 @@ function getMyPlaylistSongs(token) {
 		json: true
 	};
 
-	rp(options)
-		.then(body => {
-			let totalsongs = body.total;
-
-			for (let i = 0; i < body.items.length; i++) {
-				myplaylistobjarr.push(body.items[i].tracks);
-			}
-
-			getPlaylist(totalsongs, token)
-		})
-		.catch(error => {
-			console.error("Error");
-			console.log(error);
-		});
-}
-
-function getPlaylist(tot, toke) {
-	if (tot > 50) {
-		let prevoffset = 0;
-		let numtimes = Math.ceil((tot-50)/50);
-		let promarr = [];
-		for (let i = 0; i < numtimes; i++) {
-			let paramobj = {
-				limit: 50,
-				offset: prevoffset + 50
-			}; 
-			let params = querystring.stringify(paramobj);
-			prevoffset = paramobj.offset;
-
-			promarr.push(getPlaylistObjs(params, token));
-		}
-
-		Promise.all(promarr)
-			.then(() => {
-				console.log("woohoo");
-				console.log(myplaylistobjarr);
-				console.log(myplaylistobjarr.length);
-			})
-			.catch(error => {
-				console.log("buabuabua");
-			});
-	}
-}
-
-function getPlaylistObjs(para, token) {
-	let options = {
-		method: 'GET',
-		url: 'https://api.spotify.com/v1/me/playlists?' + para + '',
-		headers: { 'Authorization': 'Bearer ' + token },
-		json: true
-	};
-
 	return new Promise ((resolve, reject) => {
 		rp(options)
 			.then(body => {
 				let resultarr = body.items;
-
-				for (const ind in resultarr) {
-					myplaylistobjarr.push(resultarr[ind].tracks);
-				}
-				resolve(body);
+				resolve(resultarr);
 			})
 			.catch(error => {
 				console.error("Error");
@@ -269,6 +329,137 @@ function getPlaylistObjs(para, token) {
 			});
 	});
 }
+
+function getTotalPlaylistTrackObjects(inObj) {
+	let token = inObj.token;
+	let data = inObj.data;
+
+	let objArray = [];
+	let promiseArr = [];
+
+	function addToObjArr(arr) {
+		objArray.push(arr);
+		resolve("added!");
+	}
+
+	for (var i = 0; i < data.length; i++) {
+		let currentplaylist = data[i];
+		// find id
+
+		prom = new Promise((resolve, reject) => {
+			getTotalPlaylistTracks(token, id)
+			.then(getObjectsFromPlaylist)
+			.then(addToObjArr)
+
+			//Replace the upper 3 lines with:
+			//	Get total playlist tracks using the total key under tracks
+			//	getObjectsFromPlaylist
+			//	.then(addToObjArr)
+		});
+
+		promiseArr.push(prom);
+	}
+
+	return new Promise((resolve, reject) => {
+		Promise.all(promiseArr)
+		.then(() => {
+			resolve(objArray);
+		})
+	});
+}
+
+function getObjectsFromPlaylist(inObj) {
+	let token = inObj.token;
+	let totalplaylisttracks = inObj.totalplaylisttracks;
+	let id = inObj.id;
+
+	let objArray = [];
+	let promiseArr = [];
+
+	function addToObjArr(arr) {
+		objArray.push(arr);
+		resolve("added!");
+	}
+
+	let prevoffset = -50;
+	let numtimes = Math.ceil(totalplaylists/50);
+
+	for (let i = 0; i < numtimes; i++) {
+		let paramobj = {
+			limit: 50,
+			offset: prevoffset + 50
+		}; 
+		let params = querystring.stringify(paramobj);
+		prevoffset = paramobj.offset;
+
+		currentprom = new Promise((resolve, reject) => {
+			playlistTrackObjectRequest(token, params, id)
+			.then(addToObjArr)
+		});
+
+		promiseArr.push(currentprom);
+	}
+
+	return new Promise((resolve, reject) => {
+		Promise.all(promiseArr)
+			.then(() => {
+				resolve(objArray);
+			})
+			.catch(error => {
+				console.error("error");
+				reject("sorry");
+			});
+	});
+}
+
+function playlistTrackObjectRequest(token, params, id) {
+	let options = {
+		method: 'GET',
+		url: 'https://api.spotify.com/v1/me/playlists/' + id + '/tracks?' + params + '',
+		headers: { 'Authorization': 'Bearer ' + token },
+		json: true
+	};
+
+	return new Promise ((resolve, reject) => {
+		rp(options)
+			.then(body => {
+				//Convert array of playlist track objects to array of track object
+				let resultarr = body.items;
+				resolve(resultarr);
+			})
+			.catch(error => {
+				console.error("Error");
+				console.log(error);
+				reject("sorry");
+			});
+	});
+}
+
+// function getTotalPlaylistTracks(token, id) {
+// 	var options = {
+// 		method: 'GET',
+// 		url: 'https://api.spotify.com/v1/me/playlists/' + id + '/tracks?',
+// 		headers: { 'Authorization': 'Bearer ' + token },
+// 		json: true
+// 	};
+
+// 	return new Promise ((resolve, reject) => {
+// 		rp(options)
+// 		.then(body => {
+// 			console.log('Total playlists received');
+// 			let outObj = {
+// 				totalplaylisttracks: body.total,
+// 				token: token,
+// 				id: id
+// 			}
+// 			resolve(outObj);
+// 		})
+// 		.catch(err => {
+// 			reject('Error getting total saved tracks');
+// 		});
+// 	});
+// }
+
 
 app.get('/refresh_token', function(req, res) {
 
@@ -294,8 +485,9 @@ app.get('/refresh_token', function(req, res) {
   });
 });
 
-console.log('Listening on 8889');
-app.listen(8889);
+app.listen(8889, () => {
+	console.log('Listening on 8889');
+});
 
 function getAccTok(reftok) {
 	var acctok;
@@ -326,3 +518,18 @@ function getAccTok(reftok) {
 		});	
 	});
 }
+
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+ var generateRandomString = function(length) {
+	var text = '';
+	var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+	for (var i = 0; i < length; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	return text;
+ };
