016d55b6aeef5157a55eac88a83505e8998363f9
Srijan Duggal <srijanduggal17@gmail.com>
2018-05-14 18:08:29 -0500

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -11,9 +11,9 @@ var rp = require('request-promise');
 var stateKey = '__session';
 var app = express();
 
-var client_id = '15ec5ccbf8d648378ecefdf8bab3f58d'; // Your client id
-var client_secret = 'a40cc81bc12a4ea0adcb04a8638bd1f2'; // Your secret
-var redirect_uri = 'https://our-music-on-spotify.firebaseapp.com/callback/'; // Your redirect uri
+var client_id = ''; // Your client id
+var client_secret = ''; // Your secret
+var redirect_uri = ''; // Your redirect uri
 
 var firebase = require("firebase");
 
